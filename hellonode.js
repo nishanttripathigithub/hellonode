f561e8fa37dac997ddbe880e2d02f3f698dbe067
nishanttripathigithub <59622412+nishanttripathigithub@users.noreply.github.com>
2020-01-09 01:29:52 +0530

    Update hellonode.js

--- hellonode.js
+++ hellonode.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello Worlddddd')
 })
  
 app.listen(3000)
 	
-console.log('Server is now up!!')
+console.log('Server is now upppppppp!!')
